ec9eadbc5f8b85987a0a3dea06f4094fe5c42c3e
Eunjae Lee <hey@eunjae.dev>
2022-10-24 14:18:53 +0200

    fix: apply new theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,5 +4,22 @@ module.exports = {
   theme: {
     extend: {},
   },
+  daisyui: {
+    themes: [
+      {
+        lwe: {
+          primary: "#de4500",
+          secondary: "#202d85",
+          accent: "#a6e6db",
+          neutral: "#061412",
+          "base-100": "#fbf4ef",
+          info: "#a6e6db",
+          success: "#36D399",
+          warning: "#FBBD23",
+          error: "#F87272",
+        },
+      },
+    ],
+  },
   plugins: [require("daisyui")],
 };
