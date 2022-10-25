ecdece4448fcaefcb08fa73b8e01823cd4e4ba88
Eunjae Lee <hey@eunjae.dev>
2022-10-25 16:34:20 +0200

    fix: add twitter link

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,18 @@
+<script>
+  import { Twitter } from "lucide-svelte";
+</script>
+
 <div class="fixed inset-0 flex items-center justify-center">
   <div class="flex flex-col items-center">
     <h1 class="text-3xl font-bold opacity-80">Learn with Eunjae</h1>
     <p class="font-light opacity-75">Coming soon</p>
-    <div class="h-24"></div>
+    <a
+      href="https://twitter.com/learnwitheunjae"
+      target="_blank"
+      rel="noreferrer"
+      class="mt-16 text-base-100 bg-primary rounded-full p-2 opacity-25 hover:opacity-75"
+      ><Twitter size={16} /><span class="sr-only">Twitter</span></a
+    >
+    <div class="h-24" />
   </div>
 </div>
