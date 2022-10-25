f44f028c74f7609f0db49c69329023719f9e6e09
Eunjae Lee <hey@eunjae.dev>
2022-10-25 11:24:04 +0200

    fix: layout of landing (#2)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
-<div class="h-screen flex items-center justify-center">
+<div class="fixed inset-0 flex items-center justify-center">
   <div class="flex flex-col items-center">
     <h1 class="text-3xl font-bold opacity-80">Learn with Eunjae</h1>
     <p class="font-light opacity-75">Coming soon</p>
+    <div class="h-24"></div>
   </div>
 </div>
