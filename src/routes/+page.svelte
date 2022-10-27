c731dc7eae1ca8212df739374eb69764fb1f936d
Eunjae Lee <hey@eunjae.dev>
2022-10-27 14:38:50 +0200

    feat: add navbar (#3)
    * feat: add navbar
    
    * add Navbar
    
    * remove window

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,5 @@
-<div class="fixed inset-0 flex items-center justify-center">
-  <div class="flex flex-col items-center">
-    <h1 class="text-3xl font-bold opacity-80">Learn with Eunjae</h1>
-    <p class="font-light opacity-75">Coming soon</p>
-    <div class="h-24"></div>
-  </div>
-</div>
+<script>
+  import NavBar from "./NavBar.svelte";
+</script>
+
+<NavBar />
