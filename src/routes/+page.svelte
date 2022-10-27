0286802b6206ffa3d8cdec1c610e0c0ee9da2f0a
Eunjae Lee <hey@eunjae.dev>
2022-10-27 14:27:52 +0200

    add Navbar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,37 +1,5 @@
-<script lang="ts">
-  import { Menu as MenuIcon } from "lucide-svelte";
-  import {
-    Menu,
-    MenuButton,
-    MenuItems,
-    MenuItem,
-  } from "@rgossiaux/svelte-headlessui";
+<script>
+  import NavBar from "./NavBar.svelte";
 </script>
 
-<div class="navbar bg-primary text-primary-content">
-  <div class="navbar-start">
-    <a
-      href="/"
-      class="btn btn-ghost normal-case text-xl inline-block leading-none py-1"
-      ><p class="text-xs">Learn with</p>
-      <p class="font-extrabold">Eunjae</p></a
-    >
-  </div>
-  <div class="navbar-end">
-    <a href="/pd" class="btn btn-ghost normal-case">퐁당개발</a>
-    <Menu class="dropdown dropdown-end">
-      <MenuButton>More</MenuButton>
-      <MenuItems
-        as="ul"
-        class="dropdown-content menu p-2 shadow rounded-box w-36"
-      >
-        <MenuItem as="li" let:active>
-          <a href="/account-settings" class:active>Account settings</a>
-        </MenuItem>
-        <MenuItem as="li" let:active>
-          <a href="/documentation" class:active>Documentation</a>
-        </MenuItem>
-      </MenuItems>
-    </Menu>
-  </div>
-</div>
+<NavBar />
---
+++ src/routes/NavBar.svelte
@@ -0,0 +1,76 @@
+<script lang="ts">
+  import { Menu as MenuIcon } from "lucide-svelte";
+  import {
+    Menu,
+    MenuButton,
+    MenuItems,
+    MenuItem,
+  } from "@rgossiaux/svelte-headlessui";
+
+  const majorItems = [{ label: "퐁당개발", href: "/pd" }];
+
+  const minorItems = [
+    { label: "블로그", href: "/blog" },
+    { label: "발표", href: "/speak" },
+    { label: "소개", href: "/intro" },
+    { label: "연락하기", href: "/contact" },
+  ];
+
+  function isCurrentPage(href: string) {
+    return window.location.pathname === href;
+  }
+
+  function getClassForMenuItem(href: string) {
+    return isCurrentPage(href)
+      ? "underline underline-offset-4 decoration-2 decoration-wavy"
+      : "";
+  }
+</script>
+
+<div class="navbar bg-primary text-primary-content">
+  <div class="navbar-start">
+    <a
+      href="/"
+      class="btn btn-ghost normal-case text-xl inline-block leading-none py-1"
+      ><p class="text-xs">Learn with</p>
+      <p class="font-extrabold">Eunjae</p></a
+    >
+  </div>
+  <div class="navbar-end gap-2">
+    {#each majorItems as item}
+      <a
+        href={item.href}
+        class={"text-lg btn btn-ghost normal-case " +
+          getClassForMenuItem(item.href)}>{item.label}</a
+      >
+    {/each}
+
+    <Menu class="md:hidden dropdown dropdown-end">
+      <MenuButton class="btn btn-ghost normal-case"
+        ><MenuIcon /><span class="sr-only">메뉴</span></MenuButton
+      >
+      <MenuItems
+        static
+        as="ul"
+        class="text-base-content bg-base-100 dropdown-content menu p-2 shadow rounded-box w-48"
+      >
+        {#each minorItems as item}
+          <MenuItem as="li">
+            <a href={item.href} class={getClassForMenuItem(item.href)}>
+              {item.label}
+            </a>
+          </MenuItem>
+        {/each}
+      </MenuItems>
+    </Menu>
+    <ul class="hidden md:flex gap-2">
+      {#each minorItems as item}
+        <a
+          href={item.href}
+          class={"btn btn-ghost normal-case " + getClassForMenuItem(item.href)}
+          >{item.label}</a
+        >
+      {/each}
+    </ul>
+  </div>
+</div>
