f5144f8231a650a07e030f68daa52abb60785e2f
Eunjae Lee <hey@eunjae.dev>
2022-12-06 23:36:15 +0100

    disable prerender for sign_in (#5)
    * disable prerender for sign_in page
    
    * .

--- apps/web/src/routes/sign_in/+layout.ts
+++ apps/web/src/routes/sign_in/+layout.ts
@@ -1,6 +1,8 @@
 import type { LayoutLoad } from ".svelte-kit/types/src/routes/$types";
 import { redirect } from "@sveltejs/kit";
 
+export const prerender = false;
+
 export const load: LayoutLoad = async ({ parent, url }) => {
   // @ts-expect-error not typed yet
   const { session } = await parent();
