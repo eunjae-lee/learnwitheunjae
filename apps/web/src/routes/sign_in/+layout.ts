4b578eb827c21b9f262543d3dea6b67f256803c8
Eunjae Lee <hey@eunjae.dev>
2022-12-06 22:33:50 +0000

    .

--- apps/web/src/routes/sign_in/+layout.ts
+++ apps/web/src/routes/sign_in/+layout.ts
@@ -1,7 +1,7 @@
 import type { LayoutLoad } from ".svelte-kit/types/src/routes/$types";
 import { redirect } from "@sveltejs/kit";
 
-export const prerender = true;
+export const prerender = false;
 
 export const load: LayoutLoad = async ({ parent, url }) => {
   // @ts-expect-error not typed yet
