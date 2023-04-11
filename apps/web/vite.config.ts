3fc6d10fece3d79ec150b693cce5598a8356a636
Eunjae Lee <hey@eunjae.dev>
2023-04-11 09:41:18 +0200

    workaround for vite error
    https://github.com/vitejs/vite/issues/9829

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,14 +1,20 @@
-import { sveltekit } from "@sveltejs/kit/vite";
-import mkcert from "vite-plugin-mkcert";
-import type { UserConfig } from "vite";
+import { sveltekit } from '@sveltejs/kit/vite';
+import mkcert from 'vite-plugin-mkcert';
+import type { UserConfig } from 'vite';
 
 const config: UserConfig = {
   plugins: [mkcert(), sveltekit()],
   server: {
     https: true,
     fs: {
-      allow: ["content"],
+      allow: ['content'],
     },
+  },
+  define: {
+    '_process.env.XYZ': 'process.env.XYZ',
+    '_process.env.DATABASE_URL': 'process.env.DATABASE_URL',
+    '_process.env.API_ENDPOINT': 'process.env.API_ENDPOINT',
+    '_import.meta.env.XYZ': 'import.meta.env.XYZ',
   },
 };
 
